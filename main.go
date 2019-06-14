7593e226f6bd1f0e4d8bbf25b190a2fc69b172a9
budougumi0617 <budougumi0617@gmail.com>
2019-06-14 23:40:00 +0900

    :speech_balloon: Add TODO list

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import "net/url"
 
-// Result shows executed result.
-type Result struct {
+// Entry shows executed result.
+type Entry struct {
 	// Page is target URL.
 	Page url.URL
 	// HBC is Hatena Bookmark Count.
@@ -13,4 +13,25 @@ type Result struct {
 }
 
 func main() {
+	/**
+	  $ curl -D - -X GET http://api.b.st-hatena.com/entry.count?url=https%3A%2F%2Fbudougumi0617.github.io%2F2019%2F05%2F12%2Fpass-aws-solution-architect-associate%2F
+	  HTTP/1.1 200 OK
+	  Content-Type: text/plain
+	  Content-Length: 3
+	  Connection: keep-alive
+	  Date: Mon, 24 Jun 2019 12:21:51 GMT
+	  Server: nginx
+	  Cache-Control: public, max-age=3600, s-maxage=3600
+	  X-Cache: Miss from cloudfront
+	  Via: 1.1 4ca8d239c2b4b1a578fa3c7797e67c11.cloudfront.net (CloudFront)
+	  X-Amz-Cf-Pop: NRT57-C3
+	  X-Amz-Cf-Id: 3wS1whM3YI4I_PWIriHF6jGjZ5YkVXpGVAMbUSFarfz8qeUnI6osTw==
+
+	  268%
+	*/
+	// TODO build request
+	// TODO Execute GET
+	// TODO Parse response
+	// TODO show results
+	// TODO Repeat for input URLs
 }
